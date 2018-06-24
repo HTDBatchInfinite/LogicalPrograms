b14a09f018d044d124eebddf3ab0a83fb50789f9
Ujjina Chandrasekhar <ujjinachandrasekhar@gmail.com>
2018-06-24 19:29:47 +0530

    Facroial of number

---
+++ src/com/logicalcode/FactorialUseRec.java
@@ -0,0 +1,20 @@
+package com.logicalcode;
+
+public class FactorialUseRec {
+	public static void main(String[] args) {
+		int value=factorial(5);
+		System.out.println("value:::"+value);
+	}
+
+	private static int factorial(int num) {
+		if (num == 1){      
+            return 1;
+		}
+          else{      
+            return(num * factorial(num-1)); 
+          }
+		
+	}
+	
+
+}
