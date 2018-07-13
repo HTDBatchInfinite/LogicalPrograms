81e6f6ccd07d3a8a89851f4ebaf9684b854d29f6
Ujjina Chandrasekhar <ujjinachandrasekhar@gmail.com>
2018-07-13 16:45:23 +0530

    Endevour comapny Logical Coding

---
+++ src/com/logicalcode/WordCountInSentence.java
@@ -0,0 +1,23 @@
+package com.logicalcode;
+
+public class WordCountInSentence {
+public static void main(String[] args) {
+	/*String name="Test1,ALL,Test2";
+	LiteralString(name);*/
+	String name1=" chandu  ujjina  Test ";
+	int count=LiteralString(name1);
+	System.out.println("count::"+count);
+	
+}
+
+private static int LiteralString(String name) {
+	int count=0;
+	String[] wordList=name.split(" ");
+	for(String names :wordList){
+		if(!names.equals("")){
+		count=count+1;
+		}
+	}
+   return count;
+}
+}
