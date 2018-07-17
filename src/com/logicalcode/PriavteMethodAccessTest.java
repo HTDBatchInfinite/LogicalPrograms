42470ef22f4193cfd68f48116b3145aad2382d2f
Ujjina Chandrasekhar <ujjinachandrasekhar@gmail.com>
2018-07-17 15:59:34 +0530

    PriavteMethodAccessTest added

---
+++ src/com/logicalcode/PriavteMethodAccessTest.java
@@ -0,0 +1,15 @@
+package com.logicalcode;
+
+import java.lang.reflect.Method;
+
+import com.logicalcode.bean.Sample;
+
+public class PriavteMethodAccessTest {
+public static void main(String[] args) throws Exception {
+	Sample sample=new Sample();
+	Class<?> cls= sample.getClass();
+	Method method=cls.getDeclaredMethod("invokePrivate");
+	method.setAccessible(true);
+	method.invoke(sample);
+}
+}
---
+++ src/com/logicalcode/bean/Sample.java
@@ -0,0 +1,8 @@
+package com.logicalcode.bean;
+
+public class Sample {
+	private void invokePrivate(){
+		System.out.println("inside private Method");
+	}
+
+}
